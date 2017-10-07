f55ad72a64cb54590141ec84d140f9947ce5efdb
Tony Lawrence <rigatoniman@hotmail.com>
2017-10-07 14:33:27 -0700

    PracticePics

---
+++ HomeWorkWeek1/src/HWWeek1.java
@@ -0,0 +1,25 @@
+public class HWWeek1 {
+	public static void main(String[]args) {
+		reviewIntroClass();
+		System.out.println(" ");
+		aboutTony();
+	}
+	public static void reviewIntroClass() {
+		System.out.println("The intro into how we were going to operate with homework and study sounds good,");
+		System.out.println("but my only suggestion would be to expand more time on getting to know Github.");
+		System.out.println("It feels as if the skills pass you by and building up muscle memory might help....?");
+		System.out.println("Beyond that, I can't determine what else could be worked, \nother than perhaps we do workshop sessions to do the brain storm on how we create our programs and how to piece it out.");
+	}
+	public static void aboutTony() {
+		int age = 28;
+		
+		System.out.println("Hi, my name is Tony.");
+		System.out.println("I'm " + age + " years old. \nCurrently live in Kirkland, but used to live in Bellingham.");
+		System.out.println("I've worked in the QA industry for about 3 years. \nI want to learn automation so I can expand my skills as QA professional.");
+		System.out.println("For hobbies, I do gaming as a hobby and explore around the Seattle area.");
+		System.out.println("I have some experience with coding before this, but I want more exposure to it in the work field.");
+		System.out.println("Github is sort of new to me.....");
+		System.out.println("It sounds and looks simple with what the user wants, but I'm still struggling trying to get my zen mode kicking.");
+		System.out.println("Oh, and I'm a fan of Rick and Morty. \nCould really use a Mr.Meseeks right about now.");
+	}
+}
